862d4206dc5cf3f862740858e4ba831c8d7b81a7
dhelp-fr <143559596+dhelp-fr@users.noreply.github.com>
2023-10-31 22:05:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var logoBot = document.querySelector('.img-logo-bot-accueil');
+    var titleBot = document.querySelector('.title-bot');
+    
+    // Ajoutez ici le style CSS pour l'animation (par exemple, "fade-in" ou "slide-up")
+    logoBot.classList.add('fade-in');
+    titleBot.classList.add('slide-up');
+});
